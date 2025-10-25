72dbdd20d60a38043ff79c375560e4e7353c8391
Martin Laurence Coloma <cmartinlaurence@gmail.com>
2025-10-25 23:38:54 +0800

    api test 2.5 gitignore

--- api/reg/registration.ts
+++ api/reg/registration.ts
@@ -10,7 +10,6 @@ const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
-
 // --- Helper: Generate User ID ---
 function generateUserId(role: string) {
   const prefixMap: Record<string, string> = {
